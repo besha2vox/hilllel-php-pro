f4d6114b8433e6c313ba074030a439ea4799af0f
Serhii Revva <besha2vox@gmail.com>
2024-05-27 18:10:17 +0300

    complete hw8

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+interface Eater
+{
+    public function eat();
+}
+
+interface Flyer
+{
+    public function fly();
+}
+
+class Swallow implements Eater, Flyer
+{
+    public function eat()
+    {
+    }
+
+    public function fly()
+    {
+    }
+}
+
+class Ostrich implements Eater
+{
+    public function eat()
+    {
+    }
+}
