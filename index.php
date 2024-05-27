dc9f1024bf0cf06a377cd14fa805be92783d2a9f
Serhii Revva <besha2vox@gmail.com>
2024-05-27 18:24:40 +0300

    complete hw7

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+class Product
+{
+    public function get($name)
+    {
+    }
+
+    public function set($name, $value)
+    {
+    }
+}
+
+class ProductRepository
+{
+    public function save()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function delete()
+    {
+    }
+}
+
+class ProductView
+{
+    public function show()
+    {
+    }
+
+    public function print()
+    {
+    }
+}
